ce2d594490a42033a5d3dc0f2f526bfe27209248
nlukinykh <nlukinyh04@gmail.com>
2025-03-10 19:00:30 +0000

    new

--- script.py
+++ script.py
@@ -29,7 +29,6 @@ PRICE_FILE = "price.json"
 
 # Настройка Firefox
 chrome_options = Options()
-chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
